818d50726701885c412c349aa86322824cf3e528
saozdemir <43U2422a**>
2025-02-03 16:23:24 +0300

    spring-exception-handling

---
+++ exception-management/src/test/java/com/sao/ExceptionManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExceptionManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
