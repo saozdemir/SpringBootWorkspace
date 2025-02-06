f5362759a54cb7343fff5d16a8e71ce71c11b601
saozd <saozdemir1991@gmail.com>
2025-02-07 01:03:30 +0300

    spring jwt security config

---
+++ jwt-security/src/main/java/com/sao/config/SecurityConfig.java
@@ -0,0 +1,63 @@
+package com.sao.config;
+
+import com.sao.jwt.JwtAuthenticationFilter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+/**
+ * @author saozdemir
+ * @project SpringBootWorkspace
+ * @date 07 Şub 2025
+ * <p>
+ * @description: Bu sınıf erişim kontrolleri ve kayıt işlemlerini istisna tutarak contoller katmanına erişim izni
+ * vermek amacı ile kullanılacaktır.
+ */
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    public static final String AUTHENTICATE = "/authenticate";
+    public static final String REGISTER = "/register";
+
+    /**
+     * AppConfig sınıfında bu bean tanımlanmıştı burada enjekte edildi.
+     */
+    @Autowired
+    private AuthenticationProvider authenticationProvider;
+
+    /**
+     * JwtAuthenticationFilter sınıfı spring
+     */
+    @Autowired
+    private JwtAuthenticationFilter jwtAuthenticationFilter;
+
+    /**
+     * "/authenticate" ve "/register" path ine bir istek gelirse filter katmanını geçip conroller katmanına erişmesi gerekiyor.
+     * Bu metot ile bu işlemi yaptırıyoruz.
+     * Bu adımın nedeni kullanıcı adı ve şifre doğrulamasına göre bir token üretecek olmamız.
+     * Register için ise yeni kayıt olacağından mecburen filter adımını geçmesi gerekmektedir.
+     * @param http
+     * @return
+     * @throws Exception
+     */
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.csrf(csrf->csrf.disable())
+                .authorizeHttpRequests(request ->
+                        request.requestMatchers(AUTHENTICATE, REGISTER)
+                                .permitAll() /** "/authenticate" ve "/register"  adreslerine istek gelirse es geç filter'a gimesin.*/
+                                .anyRequest()
+                                .authenticated()) /** Yukarıda tanımlanan iki istisna haricindeki istekleri filter katmanında yetki kontrollerini yap*/
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authenticationProvider(authenticationProvider) /** AppConfig içinde oluşturulan AuthenticationPorvider buraya verildi.*/
+                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class); /**JwtAuthenticationFilter sınıfını enjekte ederek filter katmanının önüne koyduk.*/
+        return http.build();
+    }
+}
